0e6815affc1c7ddaf4a3561804c4345902aa09a3
imanepicduck <65570612+imanepicduck@users.noreply.github.com>
2021-12-29 17:35:03 -0500

    Add files via upload

---
+++ impossible.py
@@ -0,0 +1,18 @@
+import random
+guess = 5
+randomnumber= random.randint
+randomnumber = int(input("enter a number between 1 and 9 "))
+number = random
+chances = 5
+while chances > 0:
+    if randomnumber == number:                                                                                                                            
+        print("OH NO!! MY WINSTREAK!! IT'S BROKEN!! (ノಠ益ಠ)ノ彡┻━┻")
+        print("(The number was", number, ")")
+        break
+    elif randomnumber == number:                                                                                                        
+        print("That's not right, you  f o o l i s h   i d i o t ! ")
+        chances - 1
+        break
+    elif chances == 0:
+        print("You lose and you are a failure! No wonder you are lonely! Go eat a glass sandwich!")
+        break
